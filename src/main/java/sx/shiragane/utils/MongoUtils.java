8f00c46a1e31645935ed9a460425290c8b072f45
dreamerminsk <David70908019Miller>
2020-01-02 17:29:27 +0300

    merge

--- src/main/java/sx/shiragane/utils/MongoUtils.java
+++ src/main/java/sx/shiragane/utils/MongoUtils.java
@@ -16,11 +16,6 @@ import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class MongoUtils {
 
-    public static final MongoClient mongoClient = MongoClients.create(settings);
-    public static final MongoDatabase F_STATS = mongoClient.getDatabase("fStats");
-    public static final MongoDatabase IMDB = mongoClient.getDatabase("imdb");
-    public static final MongoCollection<Movie> IMDB_TITLES = IMDB.getCollection("titles", Movie.class);
-    public static final MongoCollection<Name> IMDB_NAMES = IMDB.getCollection("names", Name.class);
     private static final ConnectionString connString = new ConnectionString(
             "mongodb://172.105.92.75:27017/"
     );
@@ -31,6 +26,11 @@ public class MongoUtils {
             .applyConnectionString(connString)
             .retryWrites(false)
             .build();
+    public static final MongoClient mongoClient = MongoClients.create(settings);
+    public static final MongoDatabase F_STATS = mongoClient.getDatabase("fStats");
+    public static final MongoDatabase IMDB = mongoClient.getDatabase("imdb");
+    public static final MongoCollection<Movie> IMDB_TITLES = IMDB.getCollection("titles", Movie.class);
+    public static final MongoCollection<Name> IMDB_NAMES = IMDB.getCollection("names", Name.class);
 
 
 }
