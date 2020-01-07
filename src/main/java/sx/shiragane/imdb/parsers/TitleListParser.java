5bb571fa84833a079c37d4ba45ca6c224ceb2223
dKCh <tutmirage@gmail.com>
2020-01-07 20:49:07 +0300

    Create TitleListParser.java

---
+++ src/main/java/sx/shiragane/imdb/parsers/TitleListParser.java
@@ -0,0 +1,40 @@
+package sx.shiragane.imdb.parsers;
+
+import org.jsoup.nodes.Document;
+import sx.shiragane.imdb.model.Movie;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+
+public class TitleListParser {
+
+    public static List<Movie> parse(Document doc) {
+        List<Movie> movies = new ArrayList<>();
+        Movie movie = new Movie();
+        movie.setCountries(
+                        doc.select("div#titleDetails a").stream()
+                                .filter(el -> el.attr("href").contains("country_of_origin="))
+                                .map(el -> el.text().trim())
+                                .collect(Collectors.toList()));
+                doc.select("div.subtext a").stream()
+                        .filter(el -> el.attr("href").contains("releaseinfo"))
+                        .map(el -> {
+                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH);
+                            String trim = el.text().trim();
+                            String[] dts = trim.split("\\(");
+                            try {
+                                return LocalDate.parse(dts[0].trim(), formatter);
+                            } catch (Exception e) {
+                                return null;
+                            }
+                        }).filter(Objects::nonNull).findFirst().ifPresent(movie::setRelease);
+        return movies;
+    }
+
+}
