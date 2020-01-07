087fa155ff1504d78f0777cb788a1cc335afd4ad
dKCh <tutmirage@gmail.com>
2020-01-07 23:30:50 +0300

    Update TitleListParser.java
    reformat

--- src/main/java/sx/shirogane/imdb/parsers/TitleListParser.java
+++ src/main/java/sx/shirogane/imdb/parsers/TitleListParser.java
@@ -14,49 +14,49 @@ import java.util.stream.Collectors;
 
 public class TitleListParser {
 
-    public static List<Movie> parse(Document doc) {
-        List<Movie> movies = new ArrayList<>();
-          movies.addAll(doc.select("div.lister-item").stream().map(TitleListParser::parseMovie).collect(Collectors.toList()));
+    public static List < Movie > parse(Document doc) {
+        List < Movie > movies = new ArrayList < > ();
+        movies.addAll(doc.select("div.lister-item").stream().map(TitleListParser::parseMovie).collect(Collectors.toList()));
         return movies;
     }
 
     private static Movie parseMovie(Element el) {
         Movie movie = new Movie();
-        el.select(".lister-item-header a").forEach(elem -> {
+        el.select(".lister-item-header a").forEach(elem - > {
             String href = elem.attr("href");
-            Stream.of(href.split("/")).filter(p -> p.startsWith("tt"))
-                    .map(p -> p.substring(2))
-                    .mapToInt(Integer::parseInt)
-                    .forEachOrdered(movie::setIMDbID);
+            Stream.of(href.split("/")).filter(p - > p.startsWith("tt"))
+            .map(p - > p.substring(2))
+            .mapToInt(Integer::parseInt)
+            .forEachOrdered(movie::setIMDbID);
             movie.setTitle(elem.ownText().trim());
         });
-        el.select(".lister-item-year").forEach(elem -> {
+        el.select(".lister-item-year").forEach(elem - > {
             try {
                 movie.setYear(elem.ownText().trim().substring(1, 5));
             } catch (Exception e) {
                 movie.setYear("????");
             }
         });
-        el.select(".genre").forEach(elem -> {
+        el.select(".genre").forEach(elem - > {
             String ownText = elem.ownText().trim();
             movie.setGenres(Stream.of(ownText.split(",")).map(String::trim).collect(Collectors.toList()));
         });
-        el.select("div.lister-item-content p.text-muted").forEach(elem -> {
+        el.select("div.lister-item-content p.text-muted").forEach(elem - > {
             String ownText = elem.text().trim();
             System.out.println("\t" + ownText);
             movie.setDescription(ownText);
         });
-        el.select("div.lister-item-image img").forEach(elem -> {
+        el.select("div.lister-item-image img").forEach(elem - > {
             movie.setPoster(elem.attr("src"));
         });
-        el.select("p.text-muted b").forEach(elem -> {
+        el.select("p.text-muted b").forEach(elem - > {
             movie.setStatus(elem.text().trim());
         });
-        el.select("div.lister-item-content p[class='']").forEach(elem -> {
-            final List<Integer> ds = new ArrayList<Integer>();
-            final List<Integer> ss = new ArrayList<Integer>();
+        el.select("div.lister-item-content p[class='']").forEach(elem - > {
+            final List < Integer > ds = new ArrayList < Integer > ();
+            final List < Integer > ss = new ArrayList < Integer > ();
             AtomicBoolean isSpan = new AtomicBoolean(false);
-            elem.children().forEach(c -> {
+            elem.children().forEach(c - > {
                 if (c.nodeName().equalsIgnoreCase("span")) {
                     isSpan.set(true);
                 } else if (c.nodeName().equalsIgnoreCase("a")) {
@@ -73,6 +73,6 @@ public class TitleListParser {
             });
         });
         return movie;
-}
+    }
 
 }
