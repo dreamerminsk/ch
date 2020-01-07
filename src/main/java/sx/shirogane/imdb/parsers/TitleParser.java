682c529f290937e9f63530a6a40f7ff041b50bdb
dKCh <tutmirage@gmail.com>
2020-01-07 23:29:14 +0300

    Update TitleParser.java
    reformat

--- src/main/java/sx/shirogane/imdb/parsers/TitleParser.java
+++ src/main/java/sx/shirogane/imdb/parsers/TitleParser.java
@@ -17,22 +17,22 @@ public class TitleParser {
     public static Movie parse(Document doc) {
         Movie movie = new Movie();
         movie.setCountries(
-                        doc.select("div#titleDetails a").stream()
-                                .filter(el -> el.attr("href").contains("country_of_origin="))
-                                .map(el -> el.text().trim())
-                                .collect(Collectors.toList()));
-                doc.select("div.subtext a").stream()
-                        .filter(el -> el.attr("href").contains("releaseinfo"))
-                        .map(el -> {
-                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH);
-                            String trim = el.text().trim();
-                            String[] dts = trim.split("\\(");
-                            try {
-                                return LocalDate.parse(dts[0].trim(), formatter);
-                            } catch (Exception e) {
-                                return null;
-                            }
-                        }).filter(Objects::nonNull).findFirst().ifPresent(movie::setRelease);
+            doc.select("div#titleDetails a").stream()
+            .filter(el - > el.attr("href").contains("country_of_origin="))
+            .map(el - > el.text().trim())
+            .collect(Collectors.toList()));
+        doc.select("div.subtext a").stream()
+            .filter(el - > el.attr("href").contains("releaseinfo"))
+            .map(el - > {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM yyyy", Locale.ENGLISH);
+                String trim = el.text().trim();
+                String[] dts = trim.split("\\(");
+                try {
+                    return LocalDate.parse(dts[0].trim(), formatter);
+                } catch (Exception e) {
+                    return null;
+                }
+            }).filter(Objects::nonNull).findFirst().ifPresent(movie::setRelease);
         return movie;
     }
 
