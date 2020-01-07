eaa0b7cad7f13b80227072052d180d860ecc8b8a
dKCh <tutmirage@gmail.com>
2020-01-08 01:34:51 +0300

    Create NameParser.java

---
+++ src/main/java/sx/shirogane/imdb/parsers/NameParser.java
@@ -0,0 +1,21 @@
+package sx.shirogane.imdb.parsers;
+
+import org.jsoup.nodes.Document;
+import sx.shirogane.imdb.model.Name;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+
+public class NameParser {
+
+    public static Name parse(Document doc) {
+
+    }
+
+}
