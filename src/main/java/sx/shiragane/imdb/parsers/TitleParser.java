fbaa513d08b82f22f970b4911343fab9cfca4296
dKCh <tutmirage@gmail.com>
2020-01-07 20:24:39 +0300

    Create TitleParser.java

---
+++ src/main/java/sx/shiragane/imdb/parsers/TitleParser.java
@@ -0,0 +1,11 @@
+package sx.shiragane.imdb.parsers;
+
+public class TitleParser {
+
+    public static Movie parse(Document doc) {
+        Movie movie = new Movie();
+
+        return movie;
+    }
+
+}
