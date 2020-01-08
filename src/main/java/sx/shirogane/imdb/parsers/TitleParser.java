24de3f04a237f02936f183d48e57fce24a939dbf
dreamerminsk <David70908019Miller>
2020-01-08 10:34:52 +0300

    Parsers minor changes

--- src/main/java/sx/shirogane/imdb/parsers/TitleParser.java
+++ src/main/java/sx/shirogane/imdb/parsers/TitleParser.java
@@ -3,12 +3,11 @@ package sx.shirogane.imdb.parsers;
 import org.jsoup.nodes.Document;
 import sx.shirogane.imdb.model.Movie;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
-import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 
@@ -20,24 +19,24 @@ public class TitleParser {
         Movie movie = new Movie();
         movie.setCountries(parseCountries(doc));
         doc.select("div.subtext a").stream()
-            .filter(el - > el.attr("href").contains("releaseinfo"))
-            .map(el - > {
-                String trim = el.text().trim();
-                String[] dts = trim.split("\\(");
-                try {
-                    return LocalDate.parse(dts[0].trim(), formatter);
-                } catch (Exception e) {
-                    return null;
-                }
-            }).filter(Objects::nonNull).findFirst().ifPresent(movie::setRelease);
+                .filter(el -> el.attr("href").contains("releaseinfo"))
+                .map(el -> {
+                    String trim = el.text().trim();
+                    String[] dts = trim.split("\\(");
+                    try {
+                        return LocalDate.parse(dts[0].trim(), formatter);
+                    } catch (Exception e) {
+                        return null;
+                    }
+                }).filter(Objects::nonNull).findFirst().ifPresent(movie::setRelease);
         return movie;
     }
-    
+
     private static List<String> parseCountries(Document doc) {
         return doc.select("div#titleDetails a").stream()
-            .filter(el - > el.attr("href").contains("country_of_origin="))
-            .map(el - > el.text().trim())
-            .collect(Collectors.toList())
+                .filter(el -> el.attr("href").contains("country_of_origin="))
+                .map(el -> el.text().trim())
+                .collect(Collectors.toList());
     }
 
 }
